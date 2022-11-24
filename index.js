f22573404f996d0d30e8dfed823e51ac460f4ef6
Grzafnan <Grzafnan>
2022-11-24 17:03:38 +0600

    connect mongodb server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -8,6 +9,26 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = process.env.URI;
+console.log(uri);
+// aeroDb
+// rmjl13UR7u93PpTY
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// function run = async
+async function run() {
+  try {
+    await client.connect();
+    console.log('Server connected');
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+run();
 
 
 
