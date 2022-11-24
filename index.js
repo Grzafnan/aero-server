90bf82d0e90c76c09b15d6c2cb47d4cfa329345b
Grzafnan <Grzafnan>
2022-11-24 18:36:37 +0600

    implement user post request

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(express.json())
 
 
 const uri = process.env.URI;
-console.log(uri);
-// aeroDb
-// rmjl13UR7u93PpTY
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -29,6 +26,37 @@ async function run() {
   }
 }
 run();
+
+
+const Users = client.db('aero-db').collection('users');
+
+
+app.post('/users', async (req, res) => {
+  try {
+    console.log(req.body);
+    const isExists = await Users.findOne({ email: req.body.email })
+    if (isExists) {
+      return res.send({
+        success: false,
+        message: 'User already exists'
+      })
+    }
+
+    const user = await Users.insertOne(req.body);
+    console.log(user);
+
+    res.send({
+      success: true,
+      data: user
+    })
+  } catch (error) {
+    console.log(error);
+    res.send({
+      success: false,
+      error: error.message
+    })
+  }
+})
 
 
 
